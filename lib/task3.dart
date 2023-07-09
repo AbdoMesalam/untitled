52319b390b557d2ca37154fbdd2863eb4b650d17
AbdoMesalam <138803336+AbdoMesalam@users.noreply.github.com>
2023-07-10 00:43:15 +0300

    task3.dart

--- lib/task3.dart
+++ lib/task3.dart
@@ -1,138 +1,143 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MessagesScreen(),
-    );
-  }
-}
-
-class MessagesScreen extends StatefulWidget {
-  const MessagesScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
 
   @override
-  State<MessagesScreen> createState() => _MessagesScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _MessagesScreenState extends State<MessagesScreen> {
-  List<String> time = ["12:20 PM", "5:47 AM", "7:32PM", "6:40 AM", "11:00PM"];
-  List<String> names = ["Abdo", "Ahmed", "Mohamed", "Adel", "Ali"];
-  List<String> messages = [
-    "اتفرج يا صاحبي",
-    "send me your location",
-    "How are you, today",
-    "I'm busy right now, call me later",
-    "Good evening"
-  ];
-
-
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Messages",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        title: const Text("Registeration"),
+        backgroundColor: Colors.cyan,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: ListView.builder(
-            itemCount: names.length,
-            itemBuilder: (context, index) {
-              return namesItem(index);
-            }
-        ),
-      ),
-    );
-  }
-
-  Widget namesItem(int index) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+      body: Container(
+          padding: const EdgeInsets.all(10.0),
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              const CircleAvatar(
-                radius: 25,
-                backgroundImage: NetworkImage(
-                  "https://th.bing.com/th/id/R.e6e5ba63e95200efa5a20649428b258c?rik=QHK4sP9rFm30EQ&pid=ImgRaw&r=0",
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.person
+                    )
                 ),
               ),
               const SizedBox(
-                width: 20,
+                height: 20,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: 'First Name',
+                  border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.person
+                    )
+                ),
+              ),
+              const SizedBox(height: 20,),
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: 'Last Name',
+                  border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.person
+                    )
+                ),
+              ),
+              const SizedBox(height: 20,),
+
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.email
+                    )
+                ),
+              ),
+              const SizedBox(height: 20,),
+
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.phone
+                    )
+                ),
+              ),
+              const SizedBox(height: 20,),
+
+              TextFormField(
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.password
+                    )
+                ),
+              ),
+              const SizedBox(height: 20,),
+              TextFormField(
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.password
+                    )
+                ),
+              ),
+
+              const SizedBox(height: 20,),
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        names[index],
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w800,
-                        ),
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      style:
+                      ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                      onPressed: () {  },
+                      child: const Text(
+                          "Sign Up"
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: Text(
-                          time[index],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    messages[index],
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w900,
-                      color: Colors.grey,
+
+
+
                     ),
                   ),
                 ],
+
               ),
+
             ],
           ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                    Icons.arrow_forward_ios
-                ),
-              ),
-            ],
-          ),
-        ],
+        ),
+
+
+
+
       ),
+
+
+
     );
   }
 }
